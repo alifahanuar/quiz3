4dd60c01596e38bebce171153736e29f57b418bd
alifahanuar <alfhanuar@gmail.com>
2023-04-11 14:50:21 +0800

    login

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+let dbUsers = [ 
+  { 
+      username: "alifah", 
+      password: "123", 
+      name: "alifah", 
+      email: "alfhanr@gmail.com" 
+  }, 
+  { 
+      username: "hanim", 
+      password: "1234", 
+      name: "alifah", 
+      email: "hanim@gmail.com" 
+  }, 
+  { 
+      username: "fa", 
+      password: "1235", 
+      name: "fa", 
+      email: "fa@gmail.com" 
+  } 
+] 
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,7 +26,11 @@ app.use(express.json())
 
 app.post('/login', (req, res) => {
     console.log(req.body)
-  res.send('Login')
+    let result = login(
+      req.body.username,
+      req.body.password
+      )
+    res.send('Login')
 })
 
 app.get('/', (req, res) => {
@@ -18,9 +42,36 @@ app.get('/bye', (req, res) => {
   })
 
   app.post('/register', (req, res) => {
-    res.send('Account Created!')
+    let result = register(
+      req.body.username,
+      req.body.password,
+      req.body.name,
+      req.body.email
+    )
+    res.send(result)
   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function login (reqUsername,reqPassword){ 
+  let matchUser = dbUsers.find(                            //find 
+   user => user.username == reqUsername // x => console.log(x), => dia buat semua  
+   )    
+   if (!matchUser)return "User not found!" 
+   if(matchUser.password == reqPassword){ 
+       return matchUser 
+   }else{ 
+       return "Invalid password" 
+   } 
+  }
+  
+  function register(reqUsername,reqPassword,reqName,reqEmail){ 
+    dbUsers.push({                                                  //push 
+    username: reqUsername, 
+    password: reqPassword, 
+    name: reqName, 
+    email: reqEmail 
+    }) 
+} 
